39627f6d2f23a2b285a557252f1bfb0d5411f963
Amit Vikram Raj <82011740+avr2002@users.noreply.github.com>
2024-06-09 14:09:35 +0000

    feat: populated the repository from the `python-cookiecutter-template`

---
+++ tests/unit_tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for my-awesome-repo.
+
+This folder ideally has a parallel folder structure with the
+`src/my_awesome_package/` folder.
+
+In general, unit tests
+
+- should be fast and test a single function or class
+- should not depend on external resources (e.g., databases, network, etc.).
+"""
